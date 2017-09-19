5054309a0c4b5cb033a8ce50d71bd66c113662ac
Dirk Olbrich <mail@dirkolbrich.de>
2017-09-19 10:01:48 +0200

    fix importet datahandler

--- examples/buy-and-hold/main.go
+++ examples/buy-and-hold/main.go
@@ -14,7 +14,7 @@ func main() {
 	test.SetSymbols(symbols)
 
 	// create data provider and load data into the backtest
-	data := &data.BarEventFromCSVFileData{FileDir: "../testdata/test/"}
+	data := &data.BarEventFromCSVFile{FileDir: "../testdata/test/"}
 	data.Load(symbols)
 	test.SetData(data)
 

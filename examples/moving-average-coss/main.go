d179fcca90d3cefedf7fd1efad097d4671df924a
Dirk Olbrich <mail@dirkolbrich.de>
2017-09-26 14:20:27 +0200

    add sma strategy

---
+++ examples/moving-average-coss/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/dirkolbrich/gobacktest/pkg/backtest"
+	"github.com/dirkolbrich/gobacktest/pkg/data"
+	"github.com/dirkolbrich/gobacktest/pkg/strategy"
+)
+
+func main() {
+	// define symbols
+	var symbols = []string{"TEST.DE"}
+
+	// initiate new backtester and load symbols
+	test := backtest.New()
+	test.SetSymbols(symbols)
+
+	// create data provider and load data into the backtest
+	data := &data.BarEventFromCSVFile{FileDir: "../testdata/test/"}
+	data.Load(symbols)
+	test.SetData(data)
+
+	// set portfolio with initial cash and default size and risk manager
+	portfolio := &backtest.Portfolio{}
+	portfolio.SetInitialCash(10000)
+
+	sizeManager := &backtest.Size{DefaultSize: 100, DefaultValue: 1000}
+	portfolio.SetSizeManager(sizeManager)
+
+	riskManager := &backtest.Risk{}
+	portfolio.SetRiskManager(riskManager)
+
+	test.SetPortfolio(portfolio)
+
+	// create strategy provider and load into the backtest
+	strategy := &strategy.MovingAverageCross{ShortWindow: 3, LongWindow: 5}
+	test.SetStrategy(strategy)
+
+	// create execution provider and load into the backtest
+	exchange := &backtest.Exchange{Symbol: "TEST", ExchangeFee: 1.00}
+	test.SetExchange(exchange)
+
+	// choose a statistic and load into the backtest
+	statistic := &backtest.Statistic{}
+	test.SetStatistic(statistic)
+
+	// run the backtest
+	test.Run()
+
+}
---
+++ pkg/strategy/moving-average-cross.go
@@ -0,0 +1,65 @@
+package strategy
+
+import (
+	"fmt"
+
+	bt "github.com/dirkolbrich/gobacktest/pkg/backtest"
+)
+
+// MovingAverageCross is a test strategy, which interprets the SMA on a series of data events
+// specified by ShortWindow (SW) and LongWindow (LW).
+// If SW bigger tha LW and there is not already an invested BOT position, the strategy creates a buy signal.
+// If SW falls below LW and there is an invested BOT position, the strategy creates an exit signal.
+type MovingAverageCross struct {
+	ShortWindow int
+	LongWindow  int
+}
+
+// CalculateSignal handles the single Event
+func (s *MovingAverageCross) CalculateSignal(e bt.DataEventHandler, data bt.DataHandler, p bt.PortfolioHandler) (bt.SignalEvent, error) {
+	// create empty Signal
+	se := &bt.Signal{}
+
+	// type switch for event type
+	switch e := e.(type) {
+	case *bt.Bar:
+		// calculate and set SMA for short window
+		smaShort, err := bt.CalculateSMA(s.ShortWindow, data.List(e.GetSymbol()))
+		if err != nil {
+			return se, err
+		}
+		e.Metrics[fmt.Sprintf("SMA%d", s.ShortWindow)] = smaShort
+
+		// calculate and set SMA for long window
+		smaLong, err := bt.CalculateSMA(s.LongWindow, data.List(e.GetSymbol()))
+		if err != nil {
+			return se, err
+		}
+		e.Metrics[fmt.Sprintf("SMA%d", s.LongWindow)] = smaLong
+
+		// check if already invested
+		_, invested := p.IsInvested(e.GetSymbol())
+
+		if (smaShort > smaLong) && invested {
+			return se, fmt.Errorf("buy signal but already invested in %v, no signal created,", e.GetSymbol())
+		}
+
+		if (smaShort > smaLong) && !invested {
+			// buy signal, populate the signal event
+			se.Event = bt.Event{Timestamp: e.GetTime(), Symbol: e.GetSymbol()}
+			se.Direction = "long"
+		}
+
+		if (smaShort < smaLong) && !invested {
+			return se, fmt.Errorf("sell signal but not invested in %v, no signal created,", e.GetSymbol())
+		}
+
+		if (smaShort < smaLong) && invested {
+			// sell signal, populate the signal event
+			se.Event = bt.Event{Timestamp: e.GetTime(), Symbol: e.GetSymbol()}
+			se.Direction = "exit"
+		}
+
+	}
+	return se, nil
+}

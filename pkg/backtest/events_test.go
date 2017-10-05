8975ae0a459510aeca6b78f4fe151c58be92138f
Dirk Olbrich <mail@dirkolbrich.de>
2017-10-05 19:36:52 +0200

    more tests for events

--- pkg/backtest/events_test.go
+++ pkg/backtest/events_test.go
@@ -1,6 +1,7 @@
 package backtest
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -24,6 +25,96 @@ func TestTickLatestPrice(t *testing.T) {
 		float := tc.tick.LatestPrice()
 		if float != tc.exp {
 			t.Errorf("%v LatestPrice(): \nexpected %#v, \nactual %#v", tc.msg, tc.exp, float)
+		}
+	}
+}
+
+func TestSignalSetDirection(t *testing.T) {
+	var testCases = []struct {
+		msg       string
+		signal    Signal
+		dir       string
+		expSignal Signal
+	}{
+		{"simple direction:",
+			Signal{},
+			"long",
+			Signal{Direction: "long"},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.signal.SetDirection(tc.dir)
+		if !reflect.DeepEqual(tc.signal, tc.expSignal) {
+			t.Errorf("%v SetDirection(%v): \nexpected %#v, \nactual %#v",
+				tc.msg, tc.dir, tc.expSignal, tc.signal)
+		}
+	}
+}
+
+func TestSignalGetDirection(t *testing.T) {
+	var testCases = []struct {
+		msg    string
+		signal Signal
+		expDir string
+	}{
+		{"simple direction:",
+			Signal{Direction: "long"},
+			"long",
+		},
+	}
+
+	for _, tc := range testCases {
+		dir := tc.signal.GetDirection()
+		if dir != tc.expDir {
+			t.Errorf("%v GetDirection(): \nexpected %#v, \nactual %#v",
+				tc.msg, tc.expDir, dir)
+		}
+	}
+}
+
+func TestFillSetDirection(t *testing.T) {
+	var testCases = []struct {
+		msg     string
+		fill    Fill
+		dir     string
+		expFill Fill
+	}{
+		{"simple direction:",
+			Fill{},
+			"BOT",
+			Fill{Direction: "BOT"},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.fill.SetDirection(tc.dir)
+		if !reflect.DeepEqual(tc.fill, tc.expFill) {
+			t.Errorf("%v SetDirection(%v): \nexpected %#v, \nactual %#v",
+				tc.msg, tc.dir, tc.expFill, tc.fill)
+		}
+	}
+}
+
+func TestFillSetQty(t *testing.T) {
+	var testCases = []struct {
+		msg     string
+		fill    Fill
+		qty     int64
+		expFill Fill
+	}{
+		{"simple qty:",
+			Fill{},
+			100,
+			Fill{Qty: 100},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.fill.SetQty(tc.qty)
+		if !reflect.DeepEqual(tc.fill, tc.expFill) {
+			t.Errorf("%v SetQty(%v): \nexpected %#v, \nactual %#v",
+				tc.msg, tc.qty, tc.expFill, tc.fill)
 		}
 	}
 }

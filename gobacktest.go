e2e96a026e392add7b2c222230363f95c37f7613
Dirk Olbrich <mail@dirkolbrich.de>
2017-10-12 13:53:39 +0200

    add package level file for routing documentation, rename license file again

---
+++ gobacktest.go
@@ -0,0 +1,6 @@
+// Copyright 2017 Dirk Olbrich.  All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package gobacktest is a simple placeholder to root all gobacktest documentation
+package gobacktest // import "github.com/dirkolbrich/gobacktest"

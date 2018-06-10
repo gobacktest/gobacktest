46167f7b775070890ca795ec32495c48339c56d3
Dirk Olbrich <mail@dirkolbrich.de>
2018-06-10 23:22:27 +0200

    introducing commission handler

--- pkg/backtest/execution.go
+++ pkg/backtest/execution.go
@@ -1,8 +1,7 @@
 package backtest
 
 import (
-	// "fmt"
-	"github.com/dirkolbrich/gobacktest/pkg/utils"
+// "fmt"
 )
 
 // ExecutionHandler is the basic interface for executing orders
@@ -13,6 +12,7 @@ type ExecutionHandler interface {
 // Exchange is a basic execution handler implementation
 type Exchange struct {
 	Symbol      string
+	Commission  CommissionHandler
 	ExchangeFee float64
 }
 
@@ -37,30 +37,16 @@ func (e *Exchange) ExecuteOrder(order OrderEvent, data DataHandler) (*Fill, erro
 		f.Direction = "SLD"
 	}
 
-	f.Commission = e.calculateCommission(float64(f.Qty), f.Price)
+	commission, err := e.Commission.Calculate(float64(f.Qty), f.Price)
+	if err != nil {
+		return f, err
+	}
+	f.Commission = commission
+
 	f.ExchangeFee = e.calculateExchangeFee()
-	f.Cost = e.calculateCost(f.Commission, f.ExchangeFee)
+	f.Cost = e.calculateCost(commission, f.ExchangeFee)
 
 	return f, nil
-}
-
-// calculateComission() calculates the commission for a stock trade
-//
-// based on the conditions of IngDiba
-// see https://www.ing-diba.de/wertpapiere/direkt-depot/konditionen
-func (e *Exchange) calculateCommission(qty, price float64) float64 {
-	var comMin = 9.90
-	var comMax = 59.90
-	var comRate = 0.0025 // in percent
-
-	switch {
-	case (qty * price * comRate) < comMin:
-		return comMin
-	case (qty * price * comRate) > comMax:
-		return comMax
-	default:
-		return utils.Round(qty*price*comRate, 4)
-	}
 }
 
 // calculateExchangeFee() calculates the exchange fee for a stock trade

8513ccdf98bd1e7ee94db2d306d6f00130606f3c
Dirk Olbrich <mail@dirkolbrich.de>
2018-07-24 01:09:34 +0200

    refactor orderAlgo to a signalAlgo

---
+++ algo/signal.go
@@ -0,0 +1,48 @@
+package algo
+
+import (
+	gbt "github.com/dirkolbrich/gobacktest"
+)
+
+// signalAlgo creates a signal
+type signalAlgo struct {
+	gbt.Algo
+	signal    *gbt.Signal
+	direction string
+}
+
+// CreateSignal creates an signal with a specified direktion.
+func CreateSignal(direction string) gbt.AlgoHandler {
+	return &signalAlgo{direction: direction}
+}
+
+// Run runs the algo, returns the bool value of the algo
+func (algo signalAlgo) Run(s gbt.StrategyHandler) (bool, error) {
+	// data, _ := s.Data()
+	dataEvent, _ := s.Event()
+	symbol := dataEvent.Symbol()
+
+	event := &gbt.Event{}
+	event.SetTime(dataEvent.Time())
+	event.SetSymbol(symbol)
+
+	signal := &gbt.Signal{
+		Event: *event,
+	}
+
+	switch algo.direction {
+	case "BOT":
+		signal.SetDirection(gbt.BOT)
+	case "SLD":
+		signal.SetDirection(gbt.SLD)
+	case "EXT":
+		signal.SetDirection(gbt.EXT)
+	}
+
+	err := s.AddSignal(signal)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
